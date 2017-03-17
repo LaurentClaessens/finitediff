af8d43639d8eac52d46bd2977fbccbb17a7483e4
Laurent Claessens <laurent@claessens-donadello.eu>
2017-03-17 09:10:10 +0100

    Make 'deploy.sh' provide clearer output.

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,30 @@
 # One script for
 #
 # - clean 
+# - recall your git status
 # - compile all
-# - test
+# - tests
+
+function launch_test 
+{
+    echo "** Launching" $1
+    ./build/$1
+}
 
 mkdir build
 git status
 make clean
+
+echo
+echo "COMPILATION --------------------"
+echo
+
 make all
-./build/exceptions_unit_tests
-./build/repeat_function_unit_tests
-./build/multiplication_unit_tests
+
+echo
+echo "TESTS --------------------"
+echo
+
+launch_test "exceptions_unit_tests"
+launch_test "repeat_function_unit_tests"
+launch_test "multiplication_unit_tests"
