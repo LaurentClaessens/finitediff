a540ab473b479172abdd2ff7a42f20e4f6d27aa5
Laurent Claessens <laurent@claessens-donadello.eu>
2017-03-18 15:34:46 +0100

    Typo in the header protection.

--- src/SNvector.h
+++ src/SNvector.h
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef __SNVECTOR_H__170345
+#ifndef __SNVECTOR_H__170345_
 #define __SNVECTOR_H__170345_
 
 #include <array>
