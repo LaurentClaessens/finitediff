cf061a4ad440d118507cc1a625a6e864f74ceaf2
Laurent Claessens <laurent@claessens-donadello.eu>
2017-03-21 14:48:52 +0100

    Start Doxygen documentation. One first try.

--- src/SNmatrices/SNgeneric.h
+++ src/SNmatrices/SNgeneric.h
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/*
+/**
  
    This is the base class for the other matrices types.
    A matrix can be
