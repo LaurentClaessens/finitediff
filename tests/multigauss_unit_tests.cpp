7005b93fee165412739c6fdc913425480fccdac1
Laurent Claessens <laurent@claessens-donadello.eu>
2017-04-18 13:23:01 +0200

    Remove the debug information from the tests.

--- tests/multigauss_unit_tests.cpp
+++ tests/multigauss_unit_tests.cpp
@@ -327,23 +327,21 @@ ans_10E.at(3,0)=0; ans_10E.at(3,1)=24.75; ans_10E.at(3,2)=12.5; ans_10E.at(3,3)=
             ans.at(2,0)=5; ans.at(2,1)=-4;
             ans.at(3,0)=8; ans.at(3,1)=-7;
 
-            debug_matrix_print("ans",ans);
-            debug_matrix_print("iJ",iJ);
-    
             echo_single_test("the inverse is correctly computed");
             CPPUNIT_ASSERT(iJ==ans);
 
-            echo_single_test("J.inverse()*J");
-
-            debug_matrix_print("iJ",iJ);
-            debug_matrix_print("J",J);
-            debug_matrix_print("iJ*J",iJ*J);
-
-            CPPUNIT_ASSERT(ID.isNumericallyEqual(iJ*J,epsilon));
-            CPPUNIT_ASSERT(ID.isNumericallyEqual(J.inverse()*J,epsilon));
-            echo_single_test("J*J.inverse()");
+            echo_single_test("J*iJ()");
             CPPUNIT_ASSERT(ID.isNumericallyEqual(J*iJ,epsilon));
+
+            echo_single_test("J*J.inverse()");
             CPPUNIT_ASSERT(ID.isNumericallyEqual(J*J.inverse(),epsilon));
+
+            echo_single_test("iJ*J");
+            CPPUNIT_ASSERT(ID.isNumericallyEqual(iJ*J,epsilon));
+
+            echo_single_test("J.inverse()*J");
+            CPPUNIT_ASSERT(ID.isNumericallyEqual(J.inverse()*J,epsilon));
+
             echo_single_test("The inverse of the inverse");
             CPPUNIT_ASSERT(J.isNumericallyEqual(iJ.inverse(),epsilon));
         }
